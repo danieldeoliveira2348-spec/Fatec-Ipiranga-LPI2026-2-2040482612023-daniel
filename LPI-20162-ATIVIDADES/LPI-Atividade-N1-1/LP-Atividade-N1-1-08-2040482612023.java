45bf417309384a2fcc7077cffa8bb4c6da7cb165
danieldeoliveira2348-spec <danieldeoliveira2348@gmail.com>
2026-08-14 14:11:39 -0300

    adiciona o parâmetro public

--- LPI-20162-ATIVIDADES/LPI-Atividade-N1-1/LP-Atividade-N1-1-08-2040482612023.java
+++ LPI-20162-ATIVIDADES/LPI-Atividade-N1-1/LP-Atividade-N1-1-08-2040482612023.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class LP_Atividade_N1_1_08_2040482612023 {
+public class LP_Atividade_N1_1_08_2040482612023 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
